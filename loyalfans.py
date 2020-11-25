8e7fca1ad75cfda1ec1ceb974f9c002c11d117d8
Amenly <60410674+Amenly@users.noreply.github.com>
2020-11-25 17:47:15 -0600

    Update loyalfans.py

--- loyalfans.py
+++ loyalfans.py
@@ -8,20 +8,37 @@ from tqdm import tqdm
 from blessed import Terminal
 
 
+class Files:
+    files = []
+
+
 def main():
-    profile_url = "https://www.loyalfans.com/api/v2/profile/star/{}/"
-    timeline_url = "https://www.loyalfans.com/api/v2/social/timeline/{}?limit={}&page=0/"
     username = input(
         "Enter the username of the user whose page you would like to scrape:\n >>> ")
     new_profile_url = profile_url.format(username)
     print(f"Retrieving {term.bold(username)}'s page details...")
-    limit, name = scrape_profile(new_profile_url)
+    limit, name, slug = scrape_profile(new_profile_url)
+    slug_dir = os.path.join(destination, slug)
+    if os.path.isdir(slug_dir):
+        pass
+    else:
+        os.mkdir(slug_dir)
     print(f"Scraping {term.bold(name)}'s photos and videos...")
     new_timeline_url = timeline_url.format(username, limit)
     images, videos = scrape_timeline(new_timeline_url)
     if images:
-        length = len(images)
-        with tqdm(total=length, desc="Downloading photos") as bar:
+        if separate_file_types:
+            images_dir = os.path.join(slug_dir, "Images")
+            if os.path.isdir(images_dir):
+                pass
+            else:
+                os.mkdir(images_dir)
+            os.chdir(images_dir)
+        else:
+            os.chdir(slug_dir)
+        if avoid_duplicates:
+            i.files = os.listdir()
+        with tqdm(total=len(images), desc="Downloading photos") as bar:
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 futures = {executor.submit(
                     download_image, image): image for image in images}
@@ -29,15 +46,24 @@ def main():
                     future.result
                     bar.update(1)
     if videos:
-        length = len(videos)
-        with tqdm(total=length, desc="Downloading videos") as bar:
+        if separate_file_types:
+            videos_dir = os.path.join(slug_dir, "Videos")
+            if os.path.isdir(videos_dir):
+                pass
+            else:
+                os.mkdir(videos_dir)
+            os.chdir(videos_dir)
+        else:
+            os.chdir(slug_dir)
+        if avoid_duplicates:
+            v.files = os.listdir()
+        with tqdm(total=len(videos), desc="Downloading videos") as bar:
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 futures = {executor.submit(
                     download_video, video): video for video in videos}
                 for future in concurrent.futures.as_completed(futures):
                     future.result()
                     bar.update(1)
-    print(term.green("Program successfully quit"))
 
 
 def scrape_profile(url):
@@ -47,10 +73,10 @@ def scrape_profile(url):
             print(term.green(f"{r.status_code} STATUS CODE"))
         else:
             print(term.red(f"{r.status_code} STATUS CODE"))
-        content = r.content
-    json_decoded = content.decode('utf-8')
-    profile = json.loads(json_decoded)
+    profile = json.loads(r.text)
     name = profile['data']['user']['name']
+    slug = profile['data']['user']['slug']
+    # uid = profile['data']['user']['uid']
     num_posts = profile['data']['counters']['posts_total']
     num_photos = profile['data']['counters']['photos']
     num_videos = profile['data']['counters']['videos']
@@ -60,7 +86,7 @@ def scrape_profile(url):
     print(f"According to LoyalFans, {term.bold(name)} has a total of:")
     print(term.bold(
         f"\t· {num_posts} posts\n\t· {num_photos} photos\n\t· {num_videos} videos"))
-    return limit, name
+    return limit, name, slug
 
 
 def scrape_timeline(url):
@@ -70,17 +96,14 @@ def scrape_timeline(url):
             print(term.green(f"{r.status_code} STATUS CODE"))
         else:
             print(term.red(f"{r.status_code} STATUS CODE"))
-    json_decoded = r.content.decode('utf-8')
-    timeline = json.loads(json_decoded)
+    timeline = json.loads(r.text)
     posts = timeline['timeline']
     print(f"\t· Found {term.bold(str(len(posts)))} posts")
     images = []
     videos = []
     for post in posts:
-        has_photo = post['photo']
-        if has_photo:
-            has_photos = post['photos']
-            if 'photos' in has_photos:
+        if post['photo']:
+            if 'photos' in (has_photos := post['photos']):
                 photos = has_photos['photos']
                 for photo in photos:
                     image_url = photo['images']['original']
@@ -90,11 +113,8 @@ def scrape_timeline(url):
                 pass
         else:
             pass
-        has_video = post['video']
-        if has_video:
-            video_object = post['video_object']
-            videos
-            if 'video_url' in video_object:
+        if post['video']:
+            if 'video_url' in (video_object := post['video_object']):
                 video_url = video_object['video_url']
                 video_url = video_url.replace('\\', '')
                 videos.append(video_url)
@@ -114,6 +134,9 @@ def scrape_timeline(url):
 
 def download_image(url):
     filename = url.rsplit('/')[-1].split('?')[0]
+    if avoid_duplicates:
+        if filename in i.files:
+            return None
     with requests.Session() as s:
         r = s.get(url, headers=headers)
         with open(filename, 'wb') as f:
@@ -125,6 +148,9 @@ def download_video(url):
     filename = url.rsplit('/')[-1]
     if "?" in filename:
         filename = filename.split('?')[0]
+    if avoid_duplicates:
+        if filename in v.files:
+            return None
     with requests.Session() as s:
         r = s.get(url, headers=headers)
         with open(filename, 'wb') as f:
@@ -132,15 +158,21 @@ def download_video(url):
                 f.write(chunk)
 
 
-current_directory = os.getcwd()
-os.chdir(sys.path[0])
-with open('settings.json') as f:
-    headers = json.load(f)['headers']
-with open('settings.json') as f:
-    settings = json.load(f)['settings']
-    download_preview_videos = settings['download_preview_videos']
-os.chdir(current_directory)
-term = Terminal()
-
 if __name__ == '__main__':
+    settings_file = os.path.join(sys.path[0], 'config.json')
+    with open(settings_file) as f:
+        config = json.load(f)['config']
+    headers = config['headers']
+    settings = config['settings']
+    if not (destination := settings['destination_path']):
+        destination = os.getcwd()
+    separate_file_types = settings['separate_file_types']
+    download_preview_videos = settings['download_preview_videos']
+    if avoid_duplicates := settings['avoid_duplicates']:
+        i = Files()
+        v = Files()
+    profile_url = config['urls']['profile_url']
+    timeline_url = config['urls']['timeline_url']
+    term = Terminal()
     main()
+    print(term.green('Program successfully quit'))
